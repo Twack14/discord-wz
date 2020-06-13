879bc2f1182b6671fa9f10b4ba290cae6687164b
Twack14 <taylor_womack@yahoo.com>
2020-06-13 17:24:28 -0400

    updated the wz command

--- commands/wz.js
+++ commands/wz.js
@@ -1,9 +1,72 @@
+const API = require('call-of-duty-api')();
+API.login(process.env.EMAIL, process.env.PASS).then().catch();
+
 module.exports = {
     name: 'wz',
     description: 'Provide the user with their Warzone stats',
     args: true,
-    usage: 'stats <your Warzone username>',
+    usage: 'stats <platform> <your Warzone username>',
+    platforms: 'PC: battle\nSteam: steam\nXbox: xbl\nPlaystation: psn\nActivison ID: acti',
     execute(message, args) {
-       
+       if (args.length !== 3) {
+            return message.reply(`You didn't provide the correct number of arguments, ${message.author}!\nThe proper usage would be: \`!${this.name} ${this.usage}\`\nHere are the list of platforms to use:\n${this.platforms}`);
+
+
+
+        //====PC/BATTLE USERS====
+       } else if (args[1] === "battle") {
+           let wzUsername = args[2]
+           API.MWBattleData(wzUsername, API.platforms.battle).then((output) => {
+               console.log(output);
+               return message.reply(`\nWZ stats for ${wzUsername}\nTotal Battle Royale wins: ${output.br.wins}`);
+
+           }).catch((err) => {
+               console.log(err);
+           });
+
+               
+       } else if (args[1] === "psn") {
+        let wzUsername = args[2]
+        API.MWBattleData(wzUsername, API.platforms.psn).then((output) => {
+            console.log(output);
+            return message.reply(`\nWZ stats for ${wzUsername}\nTotal Battle Royale wins: ${output.br.wins}`);
+            
+        }).catch((err) => {
+            console.log(err);
+        });
+
+            
+    } else if (args[1] === "xbl") {
+        let wzUsername = args[2]
+        API.MWBattleData(wzUsername, API.platforms.xbl).then((output) => {
+            console.log(output);
+            return message.reply(`\nWZ stats for ${wzUsername}\nTotal Battle Royale wins: ${output.br.wins}`);
+            
+        }).catch((err) => {
+            console.log(err);
+        });
+
+            
+    } else if (args[1] === "steam") {
+        let wzUsername = args[2]
+        API.MWBattleData(wzUsername, API.platforms.steam).then((output) => {
+            console.log(output);
+            return message.reply(`\nWZ stats for ${wzUsername}\nTotal Battle Royale wins: ${output.br.wins}`);
+            
+        }).catch((err) => {
+            console.log(err);
+        });
+
+            
+    } else if (args[1] === "acti") {
+        let wzUsername = args[2]
+        API.MWBattleData(wzUsername, API.platforms.uno).then((output) => {
+            console.log(output);  
+            return message.reply(`\nWarzone BR stats for ${wzUsername}\nTotal Wins: ${output.br.wins}`);
+        }).catch((err) => {
+            console.log(err);
+        });
+            
+    }
     },
 };
