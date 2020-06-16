d8b09072af15747b45aa01597ceb334777be569f
Twack14 <taylor_womack@yahoo.com>
2020-06-16 17:42:05 -0400

    added cock command for dustin

---
+++ commands/cock.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'cock',
+	description: 'cock command for dustin',
+	execute(message, args) {
+		message.channel.send("https://media.giphy.com/media/l378z63MdA0vnpGr6/giphy.gif");
+	}
+};
