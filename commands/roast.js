8415ccb44799841ea9c50fdfec12ade9db2754ff
Twack14 <taylor_womack@yahoo.com>
2021-04-12 19:49:24 -0500

    new roast command

---
+++ commands/roast.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch');
+const { Channel } = require('discord.js');
+const baseUrl = "https://evilinsult.com"
+
+module.exports = {
+    name: 'roast',
+    description: 'This command displays a random insult',
+    args: true,
+    usage: '@username',
+    execute(message, args) {
+        if (args.length !== 1) {
+        return message.reply(`You didn't provide the correct number of arguments, ${message.author}!\nThe proper usage would be: \`!${this.name} ${this.usage}\`\nHere are the list of platforms to use:\n${this.platforms}`);
+        }
+
+        else {
+
+            (async () => {
+                const response = await fetch(baseUrl + "/generate_insult.php?lang=en&type=json");
+                const json = await response.json();
+                var insult = json.insult;
+                var username = args[0];
+                console.log(insult);
+                return message.channel.send(username + " " + insult);
+            })();  
+        }
+    }
+};
