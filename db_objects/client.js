ad0faf553913589d3e5957f0b63ca8657c3ee4d1
t.h.womack <taylor_womack@yahoo.com>
2021-06-21 19:29:44 -0500

    updated env variable name

--- db_objects/client.js
+++ db_objects/client.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 module.exports = {
     client: {
         host: process.env.HOST,
-        port: process.env.PORT,
+        port: process.env.DB_PORT,
         database: process.env.DATABASE,
         user: process.env.USER,
         password: process.env.DB_PASS,
