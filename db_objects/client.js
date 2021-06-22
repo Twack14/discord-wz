89984a4d5c78c2c990e65bb360e53d83f914022d
t.h.womack <taylor_womack@yahoo.com>
2021-06-21 20:25:32 -0500

    updated environment variable names

--- db_objects/client.js
+++ db_objects/client.js
@@ -5,7 +5,7 @@ module.exports = {
         host: process.env.HOST,
         port: process.env.DB_PORT,
         database: process.env.DATABASE,
-        user: process.env.USER,
+        user: process.env.DB_USER,
         password: process.env.DB_PASS,
         ssl: {
             ca: fs.readFileSync(process.env.CA)
